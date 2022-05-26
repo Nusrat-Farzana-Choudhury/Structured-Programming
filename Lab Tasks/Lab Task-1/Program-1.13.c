55a8390cb210a0f259ea43a485c8c27e225fc099
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-26 22:13:50 +0600

    Lab Task-1.13
    If a five-digit number is input through the keyboard, write a program to print a new number by adding one to each of its digits. For example, if the number that is input is 12391 then the output should be displayed as 23502.

---
+++ Lab Tasks/Lab Task-1/Program-1.13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int number,sum;
+	printf("Enter a 5 digit number: ");
+	scanf("%d",&number);
+	
+	sum=number+11111;
+	printf("The new number is: %d",sum);
+	
+  return 0;
+}
