9bfb00f335dbcac5b5ef8814a1ca727e75261d10
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-26 15:13:03 +0600

    Lab Task-1.8
     If a five-digit number is input through the keyboard, write a program to reverse the number.

---
+++ Lab Tasks/Lab Task-1/Program-1.8..c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int number,a,b,c,d,e,d1,d2,d3,d4,d5;
+	printf("Enter a 5 digit number: ");
+	scanf("%d",&number);
+	
+	a=number/10;
+	d5=number%10;
+	b=a/10;
+	d4=a%10;
+	c=b/10;
+	d3=b%10;
+	d=c/10;
+	d2=c%10;
+	e=d/10;
+	d1=d%10;
+
+    printf("The reverse orderr is: %d\t %d\t %d\t %d\t %d",d5,d4,d3,d2,d1);
+    
+	return 0;
+	
+}
