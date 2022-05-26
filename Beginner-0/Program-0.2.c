3c0e39f862db4fac8af902d05be5cce23cb008dd
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-26 09:49:20 +0600

    Using newline in a program

---
+++ Beginner-0/Program-0.2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello world!\n");
+	printf("My name is Nusrat Farzana Choudhury.\n");
+	printf("I currently study in B.Sc. in SWE, Daffodil International University.");
+	
+	return 0;
+	
+}
