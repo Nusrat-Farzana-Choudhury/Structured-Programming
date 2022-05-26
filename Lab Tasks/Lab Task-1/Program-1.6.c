26bbca13c8a8f133467d92ba71da0b63f962e431
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-26 14:26:31 +0600

    Lab Task-1.6
    Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.

---
+++ Lab Tasks/Lab Task-1/Program-1.6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float C, D;
+	
+	
+	printf("Enter the value of C:");
+	scanf("%f",&C);
+	printf("Enter the value of D:");
+	scanf("%f",&D);
+	printf("c=%.2f\nd=%.2f",D,C);
+	
+	return 0;
+	
+}
