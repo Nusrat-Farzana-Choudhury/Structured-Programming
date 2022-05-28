a228c31163ac05540a1cd9cabcd6a67ac7f96469
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-28 18:52:45 +0600

    Lab Task-2.10
    Write a program to find out your grade in the final exam. Taking the marks input from the user. Follow DIU grading method.

---
+++ Lab Tasks/Lab Task-2/Program-2.10.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+	double marks;
+	
+	printf("Enter marks: ");
+    scanf("%lf",&marks);	
+	
+	if(marks>=80 && marks<=100)
+	{
+		printf("\nGrade: A+");
+	}
+	else if(marks>=75 && marks<=79)
+    {
+	    printf("\nGrade: A");
+	}
+	else if(marks>=70 && marks<=74)
+    {
+		printf("\nGrade: A-");
+	}
+	else if(marks>=65 && marks<=69)
+    {
+		printf("\nGrade: B+");
+    }
+	else if(marks>=60 && marks<=64)
+    {
+        printf("\nGrade: B");
+    }
+	else if(marks>=55 && marks<=59)
+    {
+		printf("\nGrade: B-");
+	}
+	else if(marks>=50 && marks<=54)
+    {
+		printf("\nGrade: C+");
+	}
+	else if(marks>=45 && marks<=49)
+    {
+		printf("\nGrade: C");
+    }
+	else if(marks>=40 && marks<=44)
+    {
+		printf("\nGrade: D");
+    }
+	else
+    {
+		printf("\nGrade: F");
+    }
+	
+	return 0;
+}
