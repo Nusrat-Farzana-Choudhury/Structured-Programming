cc44bca0ccaba92c0bbf1e19d52ed0463a477823
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-05-28 19:05:32 +0600

    IF-Else
    Find a bigger number amongst 2 inputs.

---
+++ Beginner-0/Conditional Operators/0.01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b;
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("\na is bigger.");
+	}
+	else{
+		
+		printf("\nb is bigger.");
+	}
+	
+	
+	return 0;
+	
+}
