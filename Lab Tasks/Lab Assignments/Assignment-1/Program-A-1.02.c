d9fe8c0df6fa7e3b473b14c0ad7305d3a9724014
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-06 13:23:07 +0600

    Assigned Task-1.02
    Write a program in C to read 10 numbers from keyboard and find their sum and average.

--- Lab Tasks/Lab Assignments/Assignment-1/Program-A-1.02.c
+++ Lab Tasks/Lab Assignments/Assignment-1/Program-A-1.02.c
@@ -5,19 +5,19 @@ int main()
    int i,n;
    float sum=0.00, avg;
    
-   printf("Enter number 10 numbers:\n");
+   printf("Enter 10 numbers:\n");
  
    
    for (i=1;i<=10;i++)
       {
       	scanf("%d",&n);
         sum+= n;
-	    }
+      }
 	  
 	avg= sum/10;
 	  
 	printf("\n\nThe sum of the entered numbers is= %.2f",sum);
-  printf("\nThe average of the entered numbers is= %.2f\n\n",avg);	
+        printf("\nThe average of the entered numbers is= %.2f\n\n",avg);	
     
     return 0;
 }
