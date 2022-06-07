1877e844cdca110ac1b6b28549a3243a6fa0c472
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-07 15:57:57 +0600

    While

---
+++ Beginner-0/Loops/Program-0.02.c
@@ -0,0 +1,15 @@
+/*Write a C program to print first 10 natural numbers in reversed order using 'While' loop.*/
+
+#include<stdio.h>
+int main()
+{
+	int i=10;
+	
+	while(i>0)
+	{
+		printf("%d\n",i);
+		i--;
+	}
+	
+  return 0;
+}
