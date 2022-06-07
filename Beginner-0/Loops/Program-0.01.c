b9540c9576440cd24b6784a1bf5cdcc1862e2018
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-07 15:54:41 +0600

    While

---
+++ Beginner-0/Loops/Program-0.01.c
@@ -0,0 +1,16 @@
+/*Write a C program to print first 10 natural numbers using 'While' loop.*/
+
+#include <stdio.h>
+int main ()
+{
+	int i=1;
+	
+    while(i<=10)
+    {
+    	printf("%d\n",i);
+    	i++;
+    	
+  	}
+	
+    return 0;
+}
