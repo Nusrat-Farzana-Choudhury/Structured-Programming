c522dfe333caf820e692361ffefbdbb06d33ca3d
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-07 16:00:45 +0600

    For

---
+++ Beginner-0/Loops/Program-0.04.c
@@ -0,0 +1,15 @@
+/*Write a C program to print first 10 natural numbers in reversed using 'For' loop.*/
+
+#include <stdio.h>
+int main ()
+{
+	int i=10;
+	
+    for(i=10;i>0;i--)
+    {
+    	printf("%d\n",i);
+    
+    }
+	
+    return 0;
+}
