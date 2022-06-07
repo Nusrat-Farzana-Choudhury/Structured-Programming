4bf6abe70af76bc77dc2b29ce338fa965a950872
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-07 15:59:23 +0600

    For

---
+++ Beginner-0/Loops/Program-0.03.c
@@ -0,0 +1,15 @@
+/*Write a C program to print first 10 natural numbers using 'For' loop.*/
+
+#include <stdio.h>
+int main ()
+{
+	int i=1;
+	
+    for(i=1;i<=10;i++)
+    {
+    	printf("%d\n",i);
+    
+    }
+	
+    return 0;
+}
