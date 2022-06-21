5967689687e2f1519c01b607db005e122786ab35
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-21 19:58:07 +0600

    Task-03
    Write a program that takes two float number as input and show the result of addition and subtraction.

---
+++ Lab Tasks/Lab Final/Program-03.c
@@ -0,0 +1,19 @@
+/*Write a program that takes two float number as input and show the result of addition and subtraction.*/
+
+#include<stdio.h>
+int main()
+{
+	float a,b,add,sub;
+	
+	printf("Enter 2 integers:\n");
+	scanf("%f %f",&a,&b);
+	printf("\n");
+	
+	add=a+b;
+	sub=a-b;
+	
+	printf("Addition=%.2f\n",add);
+	printf("Subtraction=%.2f\n",sub);
+	
+	return 0;
+}
