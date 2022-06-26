6d7f7dba79fc0db289d1bd1fe6a11ccaea8a103b
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-26 23:35:05 +0600

    Task-09

---
+++ Lab Tasks/Lab Report/Program-19.c
@@ -0,0 +1,28 @@
+/*ID:221-35-990
+Name:Nusrat Farzana Choudhury*/
+/*Problem-19: Write a program in C to display the n terms of harmonic
+series and their sum.*/
+#include <stdio.h>
+int main()
+{
+int i,n;
+float s=0.0;
+printf("Input the number of terms : ");
+scanf("%d",&n);
+printf("\n\n");
+for(i=1; i<=n; i++)
+{
+if(i<n)
+{
+printf("1/%d + ",i);
+s+=1/(float)i;
+}
+if(i==n)
+{
+printf("1/%d ",i);
+s+=1/(float)i;
+}
+}
+printf("\nSum of Series upto %d terms : %f \n",n,s);
+return 0;
+}
