a02b7daa748ac23f72fab0631669846d00894dd0
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-27 12:58:48 +0600

    Task-01

---
+++ Lab Tasks/Lab Final/Final Submission/Program-01.c
@@ -0,0 +1,12 @@
+/*Problem-01:Write a program in C to print your name, date of birth, id and blood group.*/
+
+#include<stdio.h>
+int main()
+{
+	printf("Name\t\t: Nusrat Farzana Choudhury\n");
+	printf("Date of Birth\t: 20-09-2002\n");
+	printf("ID\t\t: 221-35-990\n");
+	printf("Blood Group\t: A(+)ve");
+	
+	return 0;
+}
