22f65bb4044f41bf79e270ef6831f44e506b49c1
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-06-27 13:12:21 +0600

    Task-02

---
+++ Lab Tasks/Lab Final/Final Submission/Program-02.c
@@ -0,0 +1,22 @@
+/*Problem-02: Write a program that takes two integers as input and print the result of addition, subtraction, multiplication & division.*/
+
+#include<stdio.h>
+int main()
+{
+	int a,b,add,sub,mul,div;
+	
+	printf("Enter 2 integers:\n");
+	scanf("%d %d",&a,&b);
+	
+	add=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	
+	printf("Addition=%d\n",add);
+	printf("Subtraction=%d\n",sub);
+	printf("Multiplication=%d\n",mul);
+	printf("Division=%d\n",div);
+	
+	return 0;
+}
