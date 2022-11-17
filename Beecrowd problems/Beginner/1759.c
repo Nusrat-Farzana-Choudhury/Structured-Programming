367f168d92c39fbd1c9fbf6e70344c7b7c66ae98
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-11-17 18:37:51 +0600

    Create 1759.c

---
+++ Beecrowd problems/Beginner/1759.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int N,i;
+	scanf("%d",&N);
+	if(0 < N <= 1000000) 
+	{
+		for(i=0;i<N-1;i++)
+		{
+			printf("Ho ");
+		}
+	}
+	
+	printf("Ho!\n");
+	
+	return 0;
+}
