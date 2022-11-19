41bac7901c00631afa7409850421ac9b49a4570e
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-11-19 13:29:07 +0600

    Create 2748.c

---
+++ Beecrowd problems/Beginner/2748.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+  printf("---------------------------------------");
+  printf("\n|        Roberto                      |");
+  printf("\n|                                     |");
+  printf("\n|        5786                         |");
+  printf("\n|                                     |");
+  printf("\n|        UNIFEI                       |");
+  printf("\n---------------------------------------\n");
+  
+    return 0;
+}
