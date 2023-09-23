26b5ebe263ac0dcc323cdc67bc556cbb5f5d94ac
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2023-09-23 20:50:13 +0600

    Create BC2755.c
    Output 9

---
+++ CP001-Practice/BC2755.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    printf("\"Ro'b'er\tto\\/\"\n"); //Use backslash(\) for escape sequence
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+    return 0;
+    
+}
